a7e7259362fcd4f03eb4b9fb3e6936ddb2ce8fde
Lynn "Zhorken" Vaughan <zhorken@gmail.com>
2012-05-20 17:38:57 -0400

    rip.sh: rip from both Sky and Blue, then merge.
    If you actually do this, you'll discover that there are a surprising
    amount of sprites that differ between Blue and Sky, including a bunch of
    standard-expression sprites — and I haven't even found most of those!
    For the most part, the differences are minor — palette tweaks, nostrils
    getting shifted by a couple pixels, etc — but it still makes this more
    difficult.  I guess I should expand this to rip /all/ the Blue sprites?
    And, if there are too many unique ones, don't bother trying to merge
    them with the Sky sprites?  Agh.

--- rip.sh
+++ rip.sh
@@ -1,20 +1,43 @@
 #!/bin/sh
 
-if [ $# -ne 2 ]
+if [ $# -ne 3 ]
 then
-    echo "Usage: rip.sh /path/to/kaomado.kao output-dir"
+    echo "Usage: rip.sh /path/to/monster.sbin /path/to/kaomado.kao output-dir"
     exit 1
 fi
 
-# Run the ripping script
-$(dirname $0)/kaomado.py $*
+monster=$1
+kaomado=$2
+output_dir=$3
+
+# Rip the Sky sprites (there are way more of those)
+$(dirname $0)/kaomado.py $kaomado $output_dir
+
+# Rip the Blue sprites to a temporary directory
+temp_dir=$(mktemp -d)
+$(dirname $0)/kaomado.py $monster $temp_dir
+
+# Compare all the Blue sprites to their Sky equivalents; keep only the Blue
+# ones that differ
+mkdir -p $output_dir/red-blue/right  # n.b. this is Gen III, so no female/
+
+for file in $(find $temp_dir -name '*.png')
+do
+    common_path=${file##$temp_dir}
+    if cmp -s $file $output_dir/$common_path
+    then
+        rm $file
+    else
+        mv $file $output_dir/red-blue/$common_path
+    fi
+done
 
 # Make duplicates for named default forms
 for form in 201-a 386-normal 412-plant 413-plant 421-overcast 422-west \
             423-west 487-altered 492-land
 do
     id=${form%%-*}
-    for file in $(find $2 -name $form.png)
+    for file in $(find $output_dir -name $form.png)
     do
         cp $file $(dirname $file)/$id.png
     done
