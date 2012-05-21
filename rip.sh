7b09d2756a9c84db6efe56abd8f10f446d11a319
Lynn "Zhorken" Vaughan <zhorken@gmail.com>
2012-05-21 12:50:31 -0400

    rip.sh: Report progress before each step.

--- rip.sh
+++ rip.sh
@@ -11,14 +11,17 @@ kaomado=$2
 output_dir=$3
 
 # Rip the Sky sprites (there are way more of those)
+echo 'Ripping Sky...'
 $(dirname $0)/kaomado.py $kaomado $output_dir
 
 # Rip the Blue sprites to a temporary directory
+echo 'Ripping Blue...'
 temp_dir=$(mktemp -d)
 $(dirname $0)/kaomado.py $monster $temp_dir
 
 # Compare all the Blue sprites to their Sky equivalents; keep only the Blue
 # ones that differ
+echo 'Comparing and merging...'
 mkdir -p $output_dir/red-blue/right  # n.b. this is Gen III, so no female/
 
 for file in $(find $temp_dir -name '*.png')
@@ -35,6 +38,7 @@ done
 rmdir $temp_dir/right $temp_dir
 
 # Make duplicates for named default forms
+echo 'Copying default forms...'
 for form in 201-a 386-normal 412-plant 413-plant 421-overcast 422-west \
             423-west 487-altered 492-land
 do
